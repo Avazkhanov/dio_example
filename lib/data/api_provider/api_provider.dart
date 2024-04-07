e819c7c3e535e1fdc5299d03a3f9568c52c21e11
avazkhanov <avazkxanovdadaxon@gmail.com>
2024-04-07 17:46:26 +0500

    fixing api_provider.dart

--- lib/data/api_provider/api_provider.dart
+++ lib/data/api_provider/api_provider.dart
@@ -1,46 +1,45 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:dio_example/data/models/users_model.dart';
 import 'package:dio_example/utils/app_constants/app_constants.dart';
 import 'package:flutter/cupertino.dart';
 
 class ApiProvider {
+  static Dio dio = Dio();
+
   static Future<List<UserModel>> getUsers() async {
     List<UserModel> users = [];
-    Dio dio = Dio();
     try {
-      Response response = await dio.get(AppConstants.baseUrl,options: Options(
-        headers: {
-
-          "Content-Type": "application/json",
-          "Authorization": "Bearer ${AppConstants.apiKey}",
-        }
-      ));
-      if (response.statusCode ==  200) {
-        users = (response.data as List?)
-                ?.map((e) => UserModel.fromJson(e))
-                .toList() ??
-            [];
+      Response response = await dio.get("${AppConstants.baseUrl}/api/v1/users",
+          options: Options(headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer ${AppConstants.apiKey}",
+          }));
+      if (response.statusCode == 200) {
+        users = (response.data['items'] as List)
+            .map((e) => UserModel.fromJson(e))
+            .toList();
       }
     } catch (error) {
-      debugPrint(error.toString());
+      debugPrint('getUsers error: $error');
     }
     return users;
   }
-  static Future<void> insertUser(UserModel userModel) async{
-    Dio dio = Dio();
-    try {
-      Response response = await dio.post(AppConstants.baseUrl,data: userModel.toJson(),options: Options(
-        headers: {
 
-          "Content-Type": "application/json",
-          "Authorization": "Bearer ${AppConstants.apiKey}",
-        }
-      ));
-      if (response.statusCode ==  200) {
+  static Future<void> insertUser(UserModel userModel) async {
+    try {
+      Response response = await dio.post("${AppConstants.baseUrl}/api/v1/users",
+          data: jsonEncode([userModel.toJson()]),
+          options: Options(headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer ${AppConstants.apiKey}",
+          }));
+      if (response.statusCode == 201) {
         print(response.data);
       }
     } catch (error) {
-      debugPrint(error.toString());
+      debugPrint('insertUser error: $error');
     }
   }
 }
